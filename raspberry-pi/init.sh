78bdd43d1df69be6ad1945611cc5cb9587c30add
Bates Westmoreland <rbwestmoreland@users.noreply.github.com>
2018-06-09 13:28:26 -0400

    Update init.sh

--- raspberry-pi/init.sh
+++ raspberry-pi/init.sh
@@ -25,6 +25,7 @@ echo Installing rbwestmoreland/scripts...
 echo ----------------------------------------
 rm -rf /usr/bin/rbwestmoreland/scripts
 git clone https://github.com/rbwestmoreland/scripts.git /usr/bin/rbwestmoreland/scripts
+find /usr/bin/rbwestmoreland/scripts/ -type f -iname "*.sh" -exec chmod +x {} \;
 grep -q "/usr/bin/rbwestmoreland/scripts/boot.sh" /etc/rc.local || sed -i -e '$i sh /usr/bin/rbwestmoreland/scripts/boot.sh\n' /etc/rc.local
 
 echo ----------------------------------------
