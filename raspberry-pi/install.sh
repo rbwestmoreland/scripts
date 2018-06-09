7395d8ecacc13f7d31c4cd3d27fcb5ea5550afd8
Bates Westmoreland <rbwestmoreland@gmail.com>
2018-06-09 15:46:41 -0400

    refactored raspberr-pi scripts

--- raspberry-pi/install.sh
+++ raspberry-pi/install.sh
@@ -3,9 +3,9 @@
 echo ----------------------------------------
 echo Installing rbwestmoreland/scripts...
 echo ----------------------------------------
-apt-get -y install git
+apt-get -qq -y install git
 rm -rf /usr/bin/rbwestmoreland/scripts
-git clone https://github.com/rbwestmoreland/scripts.git /usr/bin/rbwestmoreland/scripts
+git clone https://github.com/rbwestmoreland/scripts.git /usr/bin/rbwestmoreland/scripts --quiet
 grep -q "/usr/bin/rbwestmoreland/scripts/boot.sh" /etc/rc.local || sed -i -e '$i sh /usr/bin/rbwestmoreland/scripts/boot.sh\n' /etc/rc.local
 find /usr/bin/rbwestmoreland/scripts/ -type f -iname "*.sh" -exec chmod +x {} \;
 
