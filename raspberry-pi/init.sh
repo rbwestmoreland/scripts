e7342a092ce572030f1a400a644d87da7802b4ee
Bates Westmoreland <rbwestmoreland@users.noreply.github.com>
2018-06-09 15:00:33 -0400

    Update init.sh

--- raspberry-pi/init.sh
+++ raspberry-pi/init.sh
@@ -29,8 +29,8 @@ find /usr/bin/rbwestmoreland/scripts/ -type f -iname "*.sh" -exec chmod +x {} \;
 echo ----------------------------------------
 echo Configuring Raspberry Pi...
 echo ----------------------------------------
-bash "/usr/bin/rbwestmoreland/scripts/raspberry-pi/raspbian/configure.sh"
-bash "/usr/bin/rbwestmoreland/scripts/raspberry-pi/raspbian/reboot.sh"
+bash -ci "/usr/bin/rbwestmoreland/scripts/raspberry-pi/raspbian/configure.sh"
+bash -ci "/usr/bin/rbwestmoreland/scripts/raspberry-pi/raspbian/reboot.sh"
 
 echo ----------------------------------------
 echo Complete!
